b669cb39cb865abe80011d092b1a014b48a09ce3
Maxim Vainikka <rithmique@yahoo.com>
2021-07-12 04:52:14 +0300

    Initial Commit

---
+++ BucketListUI/BucketListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListUIApp.swift
+//  BucketListUI
+//
+//  Created by Максим Нуждин on 12.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
